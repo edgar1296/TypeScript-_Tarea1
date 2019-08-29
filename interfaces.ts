2f78bd02adeace385044fb97786878988e70c686
unknown <camachoedgar23@gmail.com>
2019-08-29 14:15:06 -0600

    IndiceFirmas

--- interfaces.ts
+++ interfaces.ts
@@ -3,26 +3,20 @@ interface Lakes {
     area: number,    
     length: number,    
     depth: number,    
-    isFreshwater: boolean,    
+    isFreshwater: boolean,   
     countries: string[],    
-    frozen?: string[] 
+    frozen?: string[],    
+    [extraProp: string]: any 
 }
  
-let secondLake: Lakes = {
-    name: 'Superior',    
-    depth: 406.3,    
-    length: 616,    
-    area: 82100,    
+let fourthLake: Lakes = {    
+    name: 'Tanganyika',    
+    depth: 1470,    
+    length: 676,    
+    area: 32600,    
     isFreshwater: true,    
-    countries: ['Canada', 'United States'] 
-}
- 
-let thirdLake: Lakes = {    
-    name: 'Baikal',    
-    depth: 1637,    
-    length: 636,    
-    area: 31500,    
-    isFreshwater: true,    
-    countries: ['Russia'],    
-    frozen: ['January', 'February', 'March', 'April', 'May']
+    countries: ['Burundi', 'Tanzania', 'Zambia', 'Congo'],    
+    kigoma:'Tanzania',    
+    kalemie: 'Congo',    
+    bujumbura: 'Burundi' 
 }
